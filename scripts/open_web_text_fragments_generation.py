b27a7cfe70131412f3131f59accd2e7e572f4d41
Fabien Roger <efab.roger@gmail.com>
2022-11-26 18:47:17 +0000

    Add open web text fragments

---
+++ scripts/open_web_text_fragments_generation.py
@@ -0,0 +1,39 @@
+#%%
+# THIS WAS RUN ON THE REDWOOD CODE BASE
+# from interp.tools.data_loading import get_val_seqs
+# from interp.tools.interpretability_tools import toks_to_string_list
+# v = get_val_seqs(n_files=1)
+# s = toks_to_string_list(v[:, 1:]) # Exclude begin token
+s = []
+
+from tqdm import tqdm
+import pandas as pd
+
+fragments = []
+for ss in tqdm(s):
+    # Exclude the start space, and select 32 words
+    fragments.append(" ".join(ss.split(" ")[1:33]))
+
+df = pd.DataFrame(fragments, columns=["fragment"])
+
+df.to_csv("../data/open_text_fragments.csv")
+#%%
+# You can still run this from here to do manual feature selection
+import pandas as pd
+
+df = pd.read_csv("../data/open_text_fragments.csv", index_col=0)
+df.head(30)
+for i, row in df.iterrows():
+    print("---", i, "---")
+    print(row.fragment)
+# %%
+# Manual selection of gender-empty texts
+gender_empty = set(range(25)) - set([5, 12, 14, 15, 18, 21, 23])
+# Manual selection of football empty text
+football_empty = set(range(25))
+last_checked = 24
+df["gender_empty"] = df.index.map(lambda i: 1 if i in gender_empty else (-1 if i <= last_checked else 0))
+df["football_empty"] = df.index.map(lambda i: 1 if i in football_empty else (-1 if i <= last_checked else 0))
+df.head(30)
+
+# %%
