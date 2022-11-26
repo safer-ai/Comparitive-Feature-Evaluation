Fix KL bug

src/dir_methods.py
@@ -231,7 +231,7 @@ def get_destruction_SGD_KL(
             inpt = tokenizer(strs, return_tensors="pt", truncation=True, max_length=inp_min_len)
             dmodel_logprobs = torch.log_softmax(destructed_model(inpt), dim=-1)
             model_logprobs = torch.log_softmax(model(**inpt).logits, dim=-1)
-            kl_loss = kl_strength * torch.nn.KLDivLoss()(dmodel_logprobs, model_logprobs)
+            kl_loss = kl_strength * torch.nn.KLDivLoss(log_target=True)(dmodel_logprobs, model_logprobs)
 
             epoch_kl_loss += kl_loss.item()
 