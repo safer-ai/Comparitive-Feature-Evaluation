043865bae435166bcb9e0c745f168d28c5a2ac7f
Fabien Roger <efab.roger@gmail.com>
2022-12-22 14:14:34 +0000

    Generate imdb

--- IMBD_prompt_extraction.py
+++ IMBD_prompt_extraction.py
@@ -6,20 +6,17 @@ from src.data_generation import PairGeneratorDataset, Pair, PairGenerator
 import numpy as np
 import json
 
-Path("data/imdb_sentiments").mkdir(parents=True, exist_ok=True)
 # %%
 df = pd.read_csv("raw_data/IMDB Dataset.csv")
 df.head()
 # %%
 train_df, test_df = train_test_split(df, test_size=0.2, random_state=0)
-train_array = train_df.to_numpy()
-val_array = test_df.to_numpy()
 # %%
 def inverse(sentiment: str) -> str:
     return "negative" if sentiment == "positive" else "positive"
 
 
-def generate_pairs(df: pd.DataFrame, n=10, n_few_shot=5, review_max_len=200):
+def generate_pairs(df: pd.DataFrame, n=10, n_few_shot=5, review_max_len=200, opposite_last=False):
     array = df[df.review.map(lambda x: len(x) < review_max_len)].to_numpy()
     print(array.shape)
 
@@ -27,24 +24,47 @@ def generate_pairs(df: pd.DataFrame, n=10, n_few_shot=5, review_max_len=200):
 
     pair_generators: list[PairGenerator] = []
 
-    positive_replacements = {"positive": ["positive"], "negative": ["negative"]}
-    negative_replacements = {"positive": ["negative"], "negative": ["positive"]}
-
     for _ in range(n):
-        indices = np.random.choice(array.shape[0], n_few_shot + 1, replace=False)
-        lines = array[indices]
-        prompt = "\n".join(
-            template.format(review=review, sentiment=sentiment)
-            for review, sentiment in lines[1:]
-        )
-        full_question = f"{prompt}\nreview: [{lines[0,0]}] sentiment:"
+        positive_replacements = {"positive": ["positive"], "negative": ["negative"]}
+        negative_replacements = {"positive": ["negative"], "negative": ["positive"]}
+        
+        if not opposite_last:
+            indices = np.random.choice(array.shape[0], n_few_shot + 1, replace=False)
+            few_shot_lines = array[indices[1:]]
+        
+            prompt = "\n".join(
+                template.format(review=review, sentiment=sentiment)
+                for review, sentiment in few_shot_lines
+            )
+            
+            correct_last_line = array[indices[0]]
+            full_question = f"{prompt}\nreview: [{correct_last_line[0]}] sentiment:"
+        
+        else:
+            indices = np.random.choice(array.shape[0], n_few_shot + 2, replace=False)
+            while array[indices][0, 1] == array[indices][1, 1]:
+                indices = np.random.choice(array.shape[0], n_few_shot + 2, replace=False)
+            few_shot_lines = array[indices[2:]]
+        
+            prompt = "\n".join(
+                template.format(review=review, sentiment=sentiment)
+                for review, sentiment in few_shot_lines
+            )
+            
+            correct_last_line = array[indices[0]]
+            wrong_last_line = array[indices[1]]
+            full_question = f"{prompt}\nreview: [{{lastreview}}] sentiment:"
+            positive_replacements["lastreview"] = [correct_last_line[0]]
+            negative_replacements["lastreview"] = [wrong_last_line[0]]
+            
+        
         pair_generators.append(
             PairGenerator(
                 full_question,
                 positive_replacements=positive_replacements,
                 negative_replacements=negative_replacements,
-                positive_answers=[" " + lines[0, 1]],
-                negative_answers=[" " + inverse(lines[0, 1])],
+                positive_answers=[" " + correct_last_line[1]],
+                negative_answers=[" " + inverse(correct_last_line[1])],
             )
         )
 
@@ -53,12 +73,29 @@ def generate_pairs(df: pd.DataFrame, n=10, n_few_shot=5, review_max_len=200):
     )
 
 
-json.dump(
-    generate_pairs(train_df, n=1000).to_dict(),
-    open("data/imdb_sentiments/train.json", "w"),
-)
-json.dump(
-    generate_pairs(test_df, n=100).to_dict(),
-    open("data/imdb_sentiments/test.json", "w"),
-)
+# Path("data/imdb_sentiments").mkdir(parents=True, exist_ok=True)
+
+# json.dump(
+#     generate_pairs(train_df, n=1000).to_dict(),
+#     open("data/imdb_sentiments/train.json", "w"),
+# )
+# json.dump(
+#     generate_pairs(test_df, n=100).to_dict(),
+#     open("data/imdb_sentiments/test.json", "w"),
+# )
+# %%
+for shot in [0, 1, 5]:
+
+    path = Path(f"data/imdb_{shot}_shot")
+    path.mkdir(parents=True, exist_ok=True)
+
+    json.dump(
+        generate_pairs(train_df, n=1000, n_few_shot=shot, opposite_last=True).to_dict(),
+        (path / "train.json").open("w"),
+    )
+    json.dump(
+        generate_pairs(test_df, n=100, n_few_shot=shot, opposite_last=True).to_dict(),
+        (path / "test.json").open("w"),
+    )
+
 # %%
