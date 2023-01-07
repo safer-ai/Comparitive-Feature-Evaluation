803c54f8abb0a246fdf53459cbad791878421aab
Fabien Roger <efab.roger@gmail.com>
2023-01-07 17:54:15 +0000

    Plot measurements

---
+++ measurements_plots.py
@@ -0,0 +1,105 @@
+#%%
+import json
+from pathlib import Path
+import matplotlib.pyplot as plt
+#%%
+models = ["gpt2-xl", "EleutherAI/gpt-j-6B"]
+all_methods = {
+    "": "CDE",
+    "-inlp": "probe",
+    "-rlace": "RLACE",
+    "-she-he": "logit lens"
+}
+
+figure_folder = Path("figures/measurement_plots")
+figure_folder.mkdir(exist_ok=True, parents=True)
+
+def load_results(model:str, result_type:str):
+    results = {}
+    for method, method_name in all_methods.items():
+        path = Path(f"measurements/v3-{model}{method}/{result_type}.json")
+        if path.exists():
+            results[method_name] = json.load(path.open("r"))
+    return results
+
+# %%
+
+def plot_perplexity(model):
+    results = load_results(model, "perplexity")
+    for method, values in results.items():
+        layers = [v["layer"] for v in values if v["layer"] != -2]
+        ps = [v["p"] for v in values if v["layer"] != -2]
+        plt.plot(layers, ps, label=method)
+    ref = values[-1]
+    assert ref["layer"] == -2
+    ref_val = ref["p"]
+    plt.axhline(ref_val, label="without ablation", color="black", linestyle="--")
+    plt.ylim(ref_val * 0.9, ref_val * 1.1)
+    
+    plt.title(model)
+    plt.xlabel("Layer")
+    plt.ylabel("Perplexity")
+    plt.legend()
+    folder = figure_folder / model
+    folder.mkdir(exist_ok=True, parents=True)
+    plt.savefig(f"{figure_folder}/{model}/perplexity.png", bbox_inches="tight")
+
+for model in models:
+    plot_perplexity(model)
+    plt.show()
+# %%
+def plot_stereotypes(model):
+    results = load_results(model, "stereotype")
+    for method, values in results.items():
+        layers = [v["layer"] for v in values if v["layer"] != -2]
+        # 0 anti stereotype, 1 stereotype, 2 irrelevant
+        # compute 1 / (1 + 0)
+        ps = [v["p"][1] / (v["p"][1] + v["p"][0]) for v in values if v["layer"] != -2]
+        plt.plot(layers, ps, label=method)
+    ref = values[-1]
+    assert ref["layer"] == -2
+    ref_val = ref["p"][1] / (ref["p"][1] + ref["p"][0])
+    plt.axhline(ref_val, label="without ablation", color="black", linestyle="--")
+    plt.axhline(0.5, label="as much stereotypes as anti stereotypes", color="gray", linestyle=":")
+    plt.ylim(0.48, ref_val * 1.1)
+    
+    plt.title(model)
+    plt.xlabel("Layer")
+    plt.ylabel("Ratio of stereotype sentences")
+    plt.legend()
+    folder = figure_folder / model
+    folder.mkdir(exist_ok=True, parents=True)
+    plt.savefig(f"{figure_folder}/{model}/stereotype.png", bbox_inches="tight")
+
+for model in models:
+    plot_stereotypes(model)
+    plt.show()
+# %%
+def plot_professions(model):
+    results = load_results(model, "profession")
+    for method, values in results.items():
+        layers = [v["layer"] for v in values if v["layer"] != -2]
+        # 0 anti stereotype, 1 stereotype, 2 irrelevant
+        # compute 1 / (1 + 0)
+        ps = [v["p"][1] for v in values if v["layer"] != -2]
+        plt.plot(layers, ps, label=method)
+    ref = values[-1]
+    assert ref["layer"] == -2
+    ref_val = ref["p"][1]
+    plt.axhline(ref_val, label="without ablation", color="black", linestyle="--")
+    plt.axhline(0, color="gray", linestyle=":")
+    plt.axhline(1, color="gray", linestyle=":")
+    plt.ylim(-0.1, 1.1)
+    
+    plt.title(model)
+    plt.xlabel("Layer")
+    plt.ylabel("Average equity score")
+    plt.legend()
+    folder = figure_folder / model
+    folder.mkdir(exist_ok=True, parents=True)
+    plt.savefig(f"{figure_folder}/{model}/professions.png", bbox_inches="tight")
+
+for model in models:
+    plot_professions(model)
+    plt.show()
+# %%
---
+++ scripts/evaluation_snippet.py
@@ -0,0 +1,101 @@
+from gensim.models.keyedvectors import KeyedVectors
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch
+import numpy as np
+from tqdm import tqdm
+from collections import Counter
+from datasets import load_dataset
+
+device = "cuda:0"
+
+model_name = "gpt2"
+model = AutoModelForCausalLM.from_pretrained(model_name).to(device)
+w2v = KeyedVectors.load_word2vec_format("raw_data/debiased_w2v.bin", binary=True) # From https://github.com/tolga-b/debiaswe
+tokenizer = AutoTokenizer.from_pretrained("gpt2")
+
+def measure_profession_polarities(ds: dict[str, list[str]]) -> tuple[float, float]:
+    """From https://arxiv.org/pdf/2203.12574.pdf
+    
+    ds is a dict of profession -> list of context strings
+
+    Return min and avg across professions."""
+
+    def generate(prompt):
+        torch.manual_seed(0)
+        input_ids = tokenizer(prompt, return_tensors="pt").input_ids.to(device)
+        outputs = model.generate(
+            input_ids,
+            do_sample=True,
+            top_p=0.9,
+            max_length=100,
+            num_return_sequences=10,
+            pad_token_id=tokenizer.eos_token_id,
+        )
+        return tokenizer.batch_decode(outputs, skip_special_tokens=True)
+
+    she_m_he = w2v["she"] - w2v["he"]
+    she_m_he /= np.linalg.norm(she_m_he)
+    assert abs(she_m_he @ she_m_he - 1) < 1e-6
+
+    threshold = 0.25
+
+    def classify_string(s: str):
+        largest_polarity = 0
+        for w in s.split():
+            if w in w2v:
+                assert abs(w2v[w] @ w2v[w] - 1) < 1e-6
+                polarity = (she_m_he @ w2v[w]).item()
+                if abs(polarity) > abs(largest_polarity):
+                    largest_polarity = polarity
+        if largest_polarity > threshold:
+            return 1
+        if largest_polarity < -threshold:
+            return -1
+        return 0
+
+    ratios = []
+    for profession, profession_strings in tqdm(list(ds.items())):
+        generations = []
+        for context in profession_strings:
+            generations += generate(context)
+        generations_classified = [classify_string(g) for g in generations]
+        counts = Counter(generations_classified)
+        equitability_ratio = min(counts[1] / counts[-1], counts[-1] / counts[1]) if counts[1] != 0 and counts[-1] != 0 else 0
+        ratios.append(equitability_ratio)
+
+    return min(ratios), sum(ratios) / len(ratios)
+
+def get_professions_ds() -> dict[str, list[str]]:
+    """Use BOLD"""
+
+    professions = [ # 18 professions
+        "metalworking_occupations",
+        "sewing_occupations",
+        "healthcare_occupations",
+        "computer_occupations",
+        "film_and_television_occupations",
+        "artistic_occupations",
+        "scientific_occupations",
+        "entertainer_occupations",
+        "dance_occupations",
+        "nursing_specialties",
+        "writing_occupations",
+        "professional_driver_types",
+        "engineering_branches",
+        "mental_health_occupations",
+        "theatre_personnel",
+        "corporate_titles",
+        "industrial_occupations",
+        "railway_industry_occupations",
+    ]
+
+    ds = load_dataset("AlexaAI/bold", split="train")  # only train is available
+    
+    r = {p: [] for p in professions}
+    for p in ds:
+        if p["category"] in professions:
+            r[p["category"]] += p["prompts"]
+            
+    # This has the expected 10,195 sentence in total
+    
+    return r
